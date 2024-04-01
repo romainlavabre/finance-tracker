0cffe09283b4f5fe349c8f22df80f9b145cdf719
romainlavabre <romainlavabre98@gmail.com>
2024-04-01 21:17:59 +0200

    [1.0.0] FIX Tests

--- src/main/java/org/romainlavabre/financetracker/crud/annuallyyield/Delete.java
+++ src/main/java/org/romainlavabre/financetracker/crud/annuallyyield/Delete.java
@@ -3,7 +3,9 @@ package org.romainlavabre.financetracker.crud.annuallyyield;
 import org.romainlavabre.financetracker.entity.AnnuallyYield;
 import org.romainlavabre.financetracker.repository.AnnuallyYieldRepository;
 import org.romainlavabre.request.Request;
+import org.springframework.stereotype.Service;
 
+@Service( "deleteAnnuallyYield" )
 public class Delete implements org.romainlavabre.crud.Delete< AnnuallyYield > {
     protected final AnnuallyYieldRepository annuallyYieldRepository;
 
